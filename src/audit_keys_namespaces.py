7a55134950894bbb89bc7ff7f2377388935975bf
Daniel Saiz <dsaiztc@gmail.com>
2015-07-10 19:03:06 +0100

    Renaming audit script files.

--- src/audit_keys_namespaces.py
+++ src/audit_keys_namespaces.py
@@ -26,27 +26,31 @@ def get_tags_with_namespace_and_without(filename):
 	lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
 	lower_colon2 = re.compile(r'^([a-z]|_)*:([a-z]|_)*:([a-z]|_)*$')
 
-	tags_with_namespace = {}
 	tags_with_namespace_and_without = {}
 
 	for event, elem in ET.iterparse(filename):
 		if elem.tag in ['node', 'way']:
+			with_namespace = set()
+			without_namespace = set()
 			for tag in elem.iter('tag'):
 				k = tag.attrib['k']
 				if lower_colon.match(k) or lower_colon2.match(k):
-					sum_to_dict(tags_with_namespace, k.split(':')[0])
-
-	for event, elem in ET.iterparse(filename):
-		if elem.tag in ['node', 'way']:
+					with_namespace.add(k.split(':')[0])
 			for tag in elem.iter('tag'):
 				k = tag.attrib['k']
 				if lower_colon.match(k) or lower_colon2.match(k):
 					pass
 				else:
-					if k in tags_with_namespace:
-						sum_to_dict(tags_with_namespace_and_without, k)
+					without_namespace.add(k)
+			my_list = list(with_namespace.intersection(without_namespace))
+			if len(my_list) != 0:
+				print my_list
+				print ET.tostring(elem)
+				print ''
+				for i in list(with_namespace.intersection(without_namespace)):
+					sum_to_dict(tags_with_namespace_and_without, i)
 
-	pprint.pprint(tags_with_namespace_and_without)
+	#pprint.pprint(tags_with_namespace_and_without)
 	return tags_with_namespace_and_without.keys()
 
 

7835cc73c52ca160f11d5af8389c0bcb74c8f43e
Daniel Saiz <dsaiztc@gmail.com>
2015-07-10 14:49:12 +0100

    Updating README with tag analysis

--- src/firstview.py
+++ src/firstview.py
@@ -2,6 +2,7 @@ import xml.etree.cElementTree as ET
 import pprint
 import sys
 import re
+import json
 
 filename = '../data/file.osm'
 
@@ -71,7 +72,7 @@ def type_of_keys(filename):
 	pprint.pprint(other)
 	return keys
 
-def type_of_keys_and_tags(filename, node_or_way='both'):
+def type_of_keys_and_tags(filename):
 
 	lower = re.compile(r'^([a-z]|_)*$')
 	lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
@@ -99,6 +100,61 @@ def type_of_keys_and_tags(filename, node_or_way='both'):
 	print '\n\nKinds of tags'
 	pprint.pprint(keys)
 
+def type_of_keys_and_tags_by_element(filename, elements=['node', 'way']):
+
+	lower = re.compile(r'^([a-z]|_)*$')
+	lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
+	lower_colon2 = re.compile(r'^([a-z]|_)*:([a-z]|_)*:([a-z]|_)*$')
+	problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
+
+	elements_dict = {k: {'lower': {}, 'lower_colon': {}, 'lower_colon2': {}, 'problemchars': {}, 'other': {}} for k in elements}
+	elements_dict_num = {k: {'lower': 0, 'lower_colon': 0, 'lower_colon2': 0, 'problemchars': 0, 'other': 0} for k in elements}
+
+	for event, elem in ET.iterparse(filename):
+		if elem.tag in elements:
+			for tag_elem in elem.iter():
+				if tag_elem.tag == 'tag':
+					k = tag_elem.attrib['k']
+					if lower.match(k):
+						elements_dict_num[elem.tag]['lower'] += 1
+						sum_to_dict(elements_dict[elem.tag]['lower'], k)
+					elif lower_colon.match(k):
+						elements_dict_num[elem.tag]['lower_colon'] += 1
+						sum_to_dict(elements_dict[elem.tag]['lower_colon'], k)
+					elif lower_colon2.match(k):
+						elements_dict_num[elem.tag]['lower_colon2'] += 1
+						sum_to_dict(elements_dict[elem.tag]['lower_colon2'], k)
+					elif problemchars.match(k):
+						elements_dict_num[elem.tag]['problemchars'] += 1
+						sum_to_dict(elements_dict[elem.tag]['problemchars'], k)
+					else:
+						elements_dict_num[elem.tag]['other'] += 1
+						sum_to_dict(elements_dict[elem.tag]['other'], k)
+	print 'Patterns on tags:\n'
+	pprint.pprint(elements_dict_num)
+	with open('../data/keys_num.json', 'w') as json_file:
+		json.dump(elements_dict_num, json_file)
+	print '\n\nKinds of tags'
+	pprint.pprint(elements_dict)
+
+	with open('../data/keys_num.json', 'w') as json_file:
+		json.dump(elements_dict_num, json_file)
+	with open('../data/keys.json', 'w') as json_file:
+		json.dump(elements_dict, json_file)
+
+def check_weird_keys(filename):
+	weirdkeys = {'node': ['CODIGO', 'FIXME', 'naptan:CommonName', 'naptan:Indicator', 'naptan:Street', 'ref:RRG'],
+				 'way': ['FIXME', 'N', u'Torre\xf3n del castillo de los Salazar', 'fuel:']}
+	for event, elem in ET.iterparse(filename):
+		if elem.tag in weirdkeys.keys():
+			for elem_tag in elem.iter():
+				if elem_tag.tag == 'tag':
+					k = elem_tag.attrib['k']
+					if k in weirdkeys[elem.tag]:
+						print ET.tostring(elem)
+						print k, elem_tag.attrib['v']
+						print ''
+
 def sum_to_dict(my_dict, value):
 	if value in my_dict:
 		my_dict[value] += 1
@@ -118,6 +174,8 @@ def main():
 	#tags = count_tags(filename)
 	#keys = type_of_keys(filename)
 	#users = unique_users(filename)
-	type_of_keys_and_tags(filename)
+	#type_of_keys_and_tags(filename)
+	#type_of_keys_and_tags_by_element(filename)
+	check_weird_keys(filename)
 
 main()
